3955a44bbf462110bd6a9ffac4d84557db4e8383
Charles Severance <csev@umich.edu>
2013-09-16 16:31:52 -0400

    Cleanup

--- index.php
+++ index.php
@@ -9,8 +9,7 @@ require_once "config.php";
 <h1>Welcome to SI664 Prototype Web Autograder</h1>
 <p>
 This is a simple autograder that runs a set of unit tests on a web site
-and then determines if a web site passes the unit tests.  This tool can 
-use IMS Learning Tools Interoperability to pass the grades back to a LMS.
+and then determines if a web site passes the unit tests. 
 </p>
 <form action="grade/assn02.php" target="_blank">
 URL to grade:
@@ -18,8 +17,10 @@ URL to grade:
 <input type="submit">
 </form>
 <p>
-If you are using this - no grades will be sent to an LMS.  To have grade routing back
-to an LMS, you need to launch this software using IMS Learning Tools Interoperability.
+This tool can use IMS Learning Tools Interoperability to pass the grades back to a LMS.
+If you are using this particlaur page, no grades will be sent to an LMS.  To have grade routing back
+to an LMS, you need to launch this software using an LMS that supports 
+IMS Learning Tools Interoperability.
 </p>
 <p>
 The source for this autograder is available at
