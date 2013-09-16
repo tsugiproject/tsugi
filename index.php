a9afb2f2c89d95df70f478ed67747cecf49e4595
Charles Severance <csev@umich.edu>
2013-09-16 16:22:25 -0400

    Dang.

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ This is a simple autograder that runs a set of unit tests on a web site
 and then determines if a web site passes the unit tests.  This tool can 
 use IMS Learning Tools Interoperability to pass the grades back to a LMS.
 </p>
-<form action="grade/assn01.php" target="_blank">
+<form action="grade/assn02.php" target="_blank">
 URL to grade:
 <input type="text" name="url" value="http://drchuck.byethost18.com/" size="100">
 <input type="submit">
