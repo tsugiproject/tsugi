3e41c7fddf25122bef95b58e031f16193d180cd6
Charles Severance <csev@umich.edu>
2013-11-21 07:56:40 -0500

    Make sure that lib/lms_lib is included

--- setup.php
+++ setup.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once $CFG->dirroot."/lib/lms_lib.php";  // During transition
+
 if ( ! defined('COOKIE_SESSION') ) {
     ini_set('session.use_cookies', '0');
     ini_set('session.use_only_cookies',0);
@@ -10,7 +12,8 @@ if ( ! isset($CFG) ) die("Please configure this product using config.php");
 if ( ! isset($CFG->staticroot) ) die('$CFG->staticroot not defined see https://github.com/csev/webauto/issues/2');
 if ( ! isset($CFG->bootstrap) ) die('$CFG->bootstrap not defined in config.php');
 if ( ! isset($CFG->timezone) ) die('$CFG->timezone not defined in config.php');
-// Set this to the temporary folder
+
+// Set this to the temporary folder if not set - dev only 
 if ( ! isset($CFG->dataroot) ) {
 	$tmp = sys_get_temp_dir();
     if (strlen($tmp) > 1 && substr($tmp, -1) == '/') $tmp = substr($tmp,0,-1);
