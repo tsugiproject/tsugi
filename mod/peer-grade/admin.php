a3fd11cab866876c869ee29d3040f8d07c0391f3
Charles Severance <csev@umich.edu>
2014-01-15 14:48:38 -0500

    Bug fixes

--- mod/peer-grade/admin.php
+++ mod/peer-grade/admin.php
@@ -16,11 +16,12 @@ $p = $CFG->dbprefix;
 
 // Gets counts and max of the submissions
 $stmt = pdoQueryDie($db,
-    "SELECT S.user_id AS user_id, email, displayname, S.submit_id as submit_id, 
+    "SELECT A.link_id, A.assn_id, S.user_id AS user_id, email, displayname, S.submit_id as submit_id, 
         MAX(points) as max_points, COUNT(points) as count_points, C.grade_count as grade_count
     FROM {$p}peer_assn AS A JOIN {$p}peer_submit as S 
+        ON A.assn_id = S.assn_id
     LEFT JOIN {$p}peer_grade AS G 
-        ON A.assn_id = S.assn_id AND S.submit_id = G.submit_id
+        ON S.submit_id = G.submit_id
     JOIN {$p}lti_user AS U
         ON S.user_id = U.user_id
     LEFT JOIN (
@@ -28,11 +29,11 @@ $stmt = pdoQueryDie($db,
         FROM {$p}peer_assn AS A JOIN {$p}peer_submit as S 
         JOIN {$p}peer_grade AS G 
             ON A.assn_id = S.assn_id AND S.submit_id = G.submit_id
-        WHERE A.link_id = 1
+        WHERE A.link_id = :LID
         GROUP BY G.user_id 
         ) AS C
         ON U.user_id = C.user_id
-    WHERE A.link_id = 1 
+    WHERE A.link_id = :LID
     GROUP BY S.submit_id
     ORDER BY S.user_id",
     array(":LID" => $LTI['link_id'])
@@ -51,7 +52,6 @@ echo('<table border="1">');
 echo("\n<tr><th>User</th><th>Email</th><th>Max Score</th><th>Scores</th><th>Graded</th></tr>\n");
 
 while ( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
-    
     echo('<tr><td><a href="student.php?user_id='.$row['user_id'].'">'.htmlent_utf8($row['displayname'])."</a></td>
         <td>".htmlent_utf8($row['email'])."</td>
         <td>".htmlent_utf8($row['max_points'])."</td><td>".htmlent_utf8($row['count_points'])."</td>
--- samples/files/files_util.php
+++ samples/files/files_util.php
@@ -23,19 +23,3 @@ function fixFileName($name)
     return $new;
 }
 
-
-// http://stackoverflow.com/questions/2840755/how-to-determine-the-max-file-upload-limit-in-php
-// http://www.kavoir.com/2010/02/php-get-the-file-uploading-limit-max-file-size-allowed-to-upload.html
-/* See also the .htaccess file.   Many MySQL servers are configured to have a max size of a 
-   blob as 1MB.  if you change the .htaccess you need to change the mysql configuration as well. 
-   this may not be possible on a low-cst provider.  */
-
-function maxUpload() {
-    $max_upload = (int)(ini_get('upload_max_filesize'));
-    $max_post = (int)(ini_get('post_max_size'));
-    $memory_limit = (int)(ini_get('memory_limit'));
-    $upload_mb = min($max_upload, $max_post, $memory_limit);
-    return $upload_mb;
-}
-
-?>
