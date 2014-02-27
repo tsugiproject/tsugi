4a36b2ea3c72ae300113353d3636ac137e3a34ca
Charles Severance <csev@umich.edu>
2014-02-27 12:09:52 -0500

    Update config-dist with MAMP example PDO string

--- config-dist.php
+++ config-dist.php
@@ -52,6 +52,7 @@ $CFG->DEVELOPER = true;
 // that can create tables.   To make the initial tables go into Admin
 // to run the upgrade.php script which auto-creates the tables.
 $CFG->pdo       = 'mysql:host=127.0.0.1;dbname=tsugi';
+# $CFG->pdo       = 'mysql:host=127.0.0.1;port=8889;dbname=tsugi'; // MAMP
 $CFG->database  = 'tsugi';
 $CFG->dbuser    = 'ltiuser';
 $CFG->dbpass    = 'ltipassword';
