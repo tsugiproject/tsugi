e4654dd458e51e0670a965602ec044176597d110
Charles Severance <csev@umich.edu>
2014-03-21 19:24:38 -0400

    Add the skeleton of the chat application.   Everything except the index.php.

---
+++ samples/chat/chatlist.php
@@ -0,0 +1,31 @@
+<?php
+require_once "../../config.php";
+require_once $CFG->dirroot."/pdo.php";
+require_once $CFG->dirroot."/lib/lms_lib.php";
+
+session_start();
+header_json();
+
+// Retrieve the launch data if present
+$LTI = requireData(array('user_id', 'result_id', 'role','context_id'));
+$p = $CFG->dbprefix;
+$displayname = $LTI['user_displayname'];
+
+// Cleanup old chats
+$stmt = $pdo->prepare("DELETE FROM {$p}sample_chat 
+		WHERE created_at < DATE_SUB( NOW(), INTERVAL 10 DAY)");
+$stmt->execute();
+
+$stmt = $pdo->prepare("SELECT chat, displayname, {$p}sample_chat.created_at AS created_at
+		FROM {$p}sample_chat JOIN {$p}lti_user 
+        ON {$p}sample_chat.user_id = {$p}lti_user.user_id 
+        WHERE link_id = :LI ORDER BY {$p}sample_chat.created_at DESC LIMIT 0,15");
+$stmt->execute(array(":LI" => $LTI['link_id']));
+
+$messages = array();
+
+while ( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
+	$messages[] = $row;
+}
+
+echo(json_encode($messages));
---
+++ samples/chat/database.php
@@ -0,0 +1,23 @@
+<?php
+
+$DATABASE_UNINSTALL = "drop table if exists {$CFG->dbprefix}sample_chat";
+
+$DATABASE_INSTALL = array(
+array( "{$CFG->dbprefix}sample_chat",
+"create table {$CFG->dbprefix}sample_chat (
+    link_id     INTEGER NOT NULL,
+    user_id     INTEGER NOT NULL,
+	chat        VARCHAR(1042) NOT NULL,
+    created_at  DATETIME NOT NULL,
+
+    CONSTRAINT `{$CFG->dbprefix}sample_chat_ibfk_1`
+        FOREIGN KEY (`link_id`)
+        REFERENCES `{$CFG->dbprefix}lti_link` (`link_id`)
+        ON DELETE CASCADE ON UPDATE CASCADE,
+
+    CONSTRAINT `{$CFG->dbprefix}sample_chat_ibfk_2`
+        FOREIGN KEY (`user_id`)
+        REFERENCES `{$CFG->dbprefix}lti_user` (`user_id`)
+        ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB DEFAULT CHARSET=utf8"));
+
