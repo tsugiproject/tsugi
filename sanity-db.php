c16dc94247dc743c6bb24fe0b7eaf18ca29f10f6
Charles Severance <csev@umich.edu>
2014-03-22 12:46:42 -0400

    More nice sanity messages.

--- sanity-db.php
+++ sanity-db.php
@@ -7,7 +7,7 @@ try {
 } catch(PDOException $ex){
     $msg = $ex->getMessage();
     error_log("DB connection: "+$msg);
-    echo('<div class="alert alert-danger" style="margin-top: 10px;">'."\n");
+    echo('<div class="alert alert-danger" style="margin: 10px;">'."\n");
     if ( strpos($msg, 'Unknown database') !== false ) {
         echo("<p>It does not appear as though your database exists.</p>
 <p> If you have full access to your MySql instance (i.e. like 
@@ -22,14 +22,51 @@ MAMP or XAMPP, you may need to run commands like this:</p>
 CPanel (or equivalent).  You must user this interface to create a database, 
 user, and password.</p>
 <p>
+In some systems, a database adminstrator will create the database,
+user, and password and simply give them to you.
+<p>
 Once you have the database, account and password you must update your
-<b>config.php</b> with this information.</p>
+<code>config.php</code> with this information.</p>
 ");
+    } else if ( strpos($msg, 'Access denied for user') !== false ) {
+        echo('<p>It appears that you have successfully created your database
+but are unable to access it due to a problem with the user and password.
+The user and password for the database conneciton are setup using either a 
+SQL <code>GRANT</code> command or created in an adminstration tool like CPanel.
+Or perhaps a system administrator created the database and gave you the
+account and password to access the database.</p>
+<p>Make sure to check the values in your <code>config.php</code> for 
+<pre>
+    $CFG->dbuser    = \'ltiuser\';
+    $CFG->dbpass    = \'ltipassword\';
+</pre>
+To make sure they match the account and password assigned to your database.
+</p>
+');
+    } else if ( strpos($msg, 'Can\'t connect to MySQL server') !== false ) {
+        echo('<p>It appears that you cannot connect to your MySQL server at 
+all.  The most likely problem is the wrong host or port in this option 
+in your <code>config.php</code> file:
+<pre>
+$CFG->pdo       = \'mysql:host=127.0.0.1;dbname=tsugi\';
+# $CFG->pdo       = \'mysql:host=127.0.0.1;port=8889;dbname=tsugi\'; // MAMP
+</pre>
+The host may be incorrect - you might try switching from \'127.0.0.1\' to 
+\'localhost\'.   Or if you are on a hosted system with an ISP the name of the 
+database host might be given to you like \'db4263.mysql.1and1.com\' and you 
+need to put that host name in the PDO string.</p>
+<p>
+Most systems are configured to use the default MySQL port of 3306 and if you 
+omit "port=" in the PDO string it assumes 3306.  If you are using MAMP
+this is usually moved to port 8889.  If neither 3306 nor 8889 works you
+probably have a bad host name.  Or talk to your system administrator.
+</p>
+');
     } else {
         echo("<p>There is a problem with your database connection.</p>\n");
-        echo("<p>Database error: ".$msg."</p>\n");
     }
 
+    echo("<p>Database error detail: ".$msg."</p>\n");
     echo("<p>Once you have fixed the problem, come back to this page and refresh
 to see if this message goes away.</p>");
     echo('<p>Installation instructions are avaiable at <a href="http://www.tsugi.org/"
@@ -37,4 +74,21 @@ target="_blank">tsugi.org</a>');
 
     echo("\n</div>\n");
     die();
+}   
+
+
+// Now check the plugins table to see if it exists
+$p = $CFG->dbprefix;
+$plugins = "{$p}lms_plugins";
+$table_fields = pdoMetadata($pdo, $plugins);
+if ( $table_fields === false ) {
+    echo('<div class="alert alert-danger" style="margin: 10px;">'."\n");
+    echo("<p>It appears that your database connection is working properly
+but you have no tables in your database.  To create the initial tables
+needed for this application, use the 'Admin' feature.  You will be prompted
+for the administrator master password as configured in <code>config.php</code>
+in the <code>\$CFG->adminpw</code> setting.
+</p>
+");
+    echo("\n</div>\n");
 }   
