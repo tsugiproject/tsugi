cd2417f5270392480ffb1ca1fbffbf8f3c91c0c6
Charles Severance <csev@umich.edu>
2014-09-15 15:37:20 -0400

    Simplify things a bit.

--- mod/attend/database.php
+++ mod/attend/database.php
@@ -1,10 +1,4 @@
 <?php
-// To allow this to be called directly or from admin/upgrade.php
-if ( !isset($PDOX) ) {
-    require_once "../../config.php";
-    $CURRENT_FILE = __FILE__;
-    require $CFG->dirroot."/admin/migrate-setup.php";
-}
 
 // The SQL to uninstall this tool
 $DATABASE_UNINSTALL = array(
@@ -35,34 +29,3 @@ array( "{$CFG->dbprefix}attend",
 ) ENGINE = InnoDB DEFAULT CHARSET=utf8")
 );
 
-// Code for the post create hook
-$DATABASE_POST_CREATE = function($table) {
-    global $CFG, $PDOX;
-
-    if ( $table == "{$CFG->dbprefix}attend") {
-        echo("If we needed a post-create hook for the attend table, it would go here.");
-    }
-
-}; // Don't forget the semicolon on anonymous functions :)
-
-// Code to do the database upgrade
-$DATABASE_UPGRADE = function($oldversion) {
-    global $CFG, $PDOX;
-
-    // A redundant ALTER TABLE sample just to show how this is done.
-    if ( $oldversion < 201408240800 ) {
-        $sql= "ALTER TABLE {$CFG->dbprefix}attend MODIFY updated_at DATETIME NOT NULL";
-        echo("Upgrading: ".$sql."<br/>\n");
-        error_log("Upgrading: ".$sql);
-        $q = $PDOX->queryDie($sql);
-    }
-
-    return 201408240800;
-
-}; // Don't forget the semicolon on anonymous functions :)
-
-// Do the actual migration if we are not in admin/upgrade.php
-if ( isset($CURRENT_FILE) ) {
-    include $CFG->dirroot."/admin/migrate-run.php";
-    $OUTPUT->footer();
-}
