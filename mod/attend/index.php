5c7dff3d5f1ccb4fa1db3b76f002d204dba54f61
Charles Severance <csev@umich.edu>
2014-09-15 12:19:38 -0400

    Make the attend code more presentable as a developer example.

--- mod/attend/index.php
+++ mod/attend/index.php
@@ -3,38 +3,39 @@ require_once "../../config.php";
 require_once $CFG->dirroot."/pdo.php";
 require_once $CFG->dirroot."/lib/lms_lib.php";
 
+use \Tsugi\Core\Settings;
+
+// Initalize and check the Session details
 $LTI = \Tsugi\Core\LTIX::requireData(array('user_id', 'link_id', 'role','context_id'));
 
 // Model
 $p = $CFG->dbprefix;
-$stmt = $PDOX->prepare("SELECT code FROM {$p}attend_code WHERE link_id = :ID");
-$stmt->execute(array(":ID" => $LINK->id));
-$row = $stmt->fetch(PDO::FETCH_ASSOC);
-$old_code = "";
-if ( $row !== false ) $old_code = $row['code'];
+$old_code = Settings::linkGet('code', '');
 
-if ( isset($_POST['code']) && $USER->instructor ) {
-    $sql = "INSERT INTO {$p}attend_code
-            (link_id, code) VALUES ( :ID, :CO )
-            ON DUPLICATE KEY UPDATE code = :CO";
-    $stmt = $PDOX->prepare($sql);
-    $stmt->execute(array(
-        ':CO' => $_POST['code'],
-        ':ID' => $LINK->id));
+if ( isset($_POST['code']) && isset($_POST['set']) && $USER->instructor ) {
+    Settings::linkSet('code', $_POST['code']);
     $_SESSION['success'] = 'Code updated';
+    header( 'Location: '.addSession('index.php') ) ;
+    return;
+} else if ( isset($_POST['clear']) && $USER->instructor ) {
+    $rows = $PDOX->queryDie("DELETE FROM {$p}attend WHERE link_id = :LI",
+            array(':LI' => $LINK->id)
+    );
+    $_SESSION['success'] = 'Data cleared';
     header( 'Location: '.addSession('index.php') ) ;
     return;
 } else if ( isset($_POST['code']) ) { // Student
     if ( $old_code == $_POST['code'] ) {
-        $sql = "INSERT INTO {$p}attend
+        $PDOX->queryDie("INSERT INTO {$p}attend
             (link_id, user_id, ipaddr, attend, updated_at)
             VALUES ( :LI, :UI, :IP, NOW(), NOW() )
-            ON DUPLICATE KEY UPDATE updated_at = NOW()";
-        $stmt = $PDOX->prepare($sql);
-        $stmt->execute(array(
-            ':LI' => $LINK->id,
-            ':UI' => $USER->id,
-            ':IP' => $_SERVER["REMOTE_ADDR"]));
+            ON DUPLICATE KEY UPDATE updated_at = NOW()",
+            array(
+                ':LI' => $LINK->id,
+                ':UI' => $USER->id,
+                ':IP' => $_SERVER["REMOTE_ADDR"]
+            )
+        );
         $_SESSION['success'] = _('Attendance Recorded...');
     } else {
         $_SESSION['error'] = _('Code incorrect');
@@ -49,30 +50,33 @@ $OUTPUT->bodyStart();
 $OUTPUT->flashMessages();
 $OUTPUT->welcomeUserCourse();
 
+// We could use the settings form - but we will keep this simple
 echo('<form method="post">');
 echo(_("Enter code:")."\n");
 if ( $USER->instructor ) {
-echo('<input type="text" name="code" value="'.htmlent_utf8($old_code).'"> ');
-echo('<input type="submit" name="send" value="'._('Update code').'"><br/>');
+    echo('<input type="text" name="code" value="'.htmlent_utf8($old_code).'"> ');
+    echo('<input type="submit" class="btn btn-normal" name="set" value="'._('Update code').'"> ');
+    echo('<input type="submit" class="btn btn-warning" name="clear" value="'._('Clear data').'"><br/>');
 } else {
-echo('<input type="text" name="code" value=""> ');
-echo('<input type="submit" name="send" value="'._('Record attendance').'"><br/>');
+    echo('<input type="text" name="code" value=""> ');
+    echo('<input type="submit" class="btn btn-normal" name="set" value="'._('Record attendance').'"><br/>');
 }
 echo("\n</form>\n");
 
 if ( $USER->instructor ) {
-    $stmt = $PDOX->prepare("SELECT user_id,attend,ipaddr FROM {$p}attend
-            WHERE link_id = :LI ORDER BY attend DESC, user_id");
-    $stmt->execute(array(':LI' => $LINK->id));
+    $rows = $PDOX->allRowsDie("SELECT user_id,attend,ipaddr FROM {$p}attend
+            WHERE link_id = :LI ORDER BY attend DESC, user_id",
+            array(':LI' => $LINK->id)
+    );
     echo('<table border="1">'."\n");
     echo("<tr><th>"._("User")."</th><th>"._("Attendance")."</th><th>"._("IP Address")."</th></tr>\n");
-    while ( $row = $stmt->fetch(PDO::FETCH_ASSOC) ) {
+    foreach ( $rows as $row ) {
         echo "<tr><td>";
         echo($row['user_id']);
         echo("</td><td>");
         echo($row['attend']);
         echo("</td><td>");
-        echo(htmlentities($row['ipaddr']));
+        echo(htmlent_utf8($row['ipaddr']));
         echo("</td></tr>\n");
     }
     echo("</table>\n");
