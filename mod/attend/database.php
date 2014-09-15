7961d5a3462e101204c0b27bcb0308cfabfaca5f
Charles Severance <csev@umich.edu>
2014-09-15 12:23:33 -0400

    Fix the database.php stuff to reflect the new attendance model.

--- mod/attend/database.php
+++ mod/attend/database.php
@@ -1,7 +1,6 @@
 <?php
 
-$DATABASE_INSTALL = array(
-"drop table if exists {$CFG->dbprefix}attend",
+$DATABASE_UNINSTALL = array(
 "drop table if exists {$CFG->dbprefix}attend_code"
 );
 
@@ -18,27 +17,6 @@ array( "{$CFG->dbprefix}attend_code",
         ON DELETE CASCADE ON UPDATE CASCADE,
 
     UNIQUE(link_id)
-) ENGINE = InnoDB DEFAULT CHARSET=utf8"),
-
-array( "{$CFG->dbprefix}attend",
-"create table {$CFG->dbprefix}attend (
-    link_id     INTEGER NOT NULL,
-    user_id     INTEGER NOT NULL,
-    attend      DATE NOT NULL,
-    ipaddr      varchar(64),
-    updated_at  DATETIME NOT NULL,
-
-    CONSTRAINT `{$CFG->dbprefix}attend_ibfk_1`
-        FOREIGN KEY (`link_id`)
-        REFERENCES `{$CFG->dbprefix}lti_link` (`link_id`)
-        ON DELETE CASCADE ON UPDATE CASCADE,
-
-    CONSTRAINT `{$CFG->dbprefix}attend_ibfk_2`
-        FOREIGN KEY (`user_id`)
-        REFERENCES `{$CFG->dbprefix}lti_user` (`user_id`)
-        ON DELETE CASCADE ON UPDATE CASCADE,
-
-    UNIQUE(link_id, user_id, attend)
 ) ENGINE = InnoDB DEFAULT CHARSET=utf8")
 );
 
