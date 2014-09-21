e4de00950bc2f0a6b5b295b70830a266982cd907
Charles Severance <csev@umich.edu>
2014-09-21 12:08:43 -0400

    Eliminate gradeSend() - add result_id to the $LINK variable.

--- Tsugi/Core/Link.php
+++ Tsugi/Core/Link.php
@@ -5,7 +5,7 @@ namespace Tsugi\Core;
 /**
  * This is a class to provide access to the resource link level data.
  *
- * This data comes from the LTI launch from the LMS. 
+ * This data comes from the LTI launch from the LMS.
  * A resource_link may or may not be in a context.  If there
  * is a link without a context, it is a "system-wide" link
  * like "view profile" or "show all courses"
@@ -16,12 +16,25 @@ class Link {
      * The integer primary key for this link in the 'lti_link' table.
      */
     public $id;
+
     /**
      * The string primary key for this link in the 'lti_link' table.
      */
     public $sha256;
+
     /**
      * The link title
      */
     public $title;
+
+    /**
+     * The result_id for the link (if set)
+     *
+     * This is the primary key for the lti_result row for this
+     * user/link combination.  It may be false.  Is this is not
+     * false, we can send a grade back to the LMS for this
+     * user/link combination.
+     */
+    public $result_id = false;
+
 }
