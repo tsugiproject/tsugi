9153f0b70bc684b0545d784333f409690f05d5eb
Charles Severance <csev@umich.edu>
2014-10-02 17:48:27 -0400

    The first simple PHP Unit Test

---
+++ lib/vendor/Tsugi/Util/NetTest.php
@@ -0,0 +1,24 @@
+<?php
+
+require "Net.php";
+
+use \Tsugi\Util\Net;
+
+class TestTest extends PHPUnit_Framework_TestCase
+{
+    public function testGet() {
+        $stuff = Net::doGet("http://www.dr-chuck.com/page1.htm");
+        $this->assertContains("The First Page",$stuff);
+    }
+
+    public function testGetStream() {
+        $stuff = Net::getStream("http://www.dr-chuck.com/page1.htm");
+        $this->assertContains("The First Page",$stuff);
+    }
+
+    public function testGetCurl() {
+        $stuff = Net::getCurl("http://www.dr-chuck.com/page1.htm");
+        $this->assertContains("The First Page",$stuff);
+    }
+
+}
