7d8c1c712fc413c7140a78a2b459c8e8986e0238
Charles R. Severance <csev@umich.edu>
2014-12-30 12:27:48 -0500

    Add support for universal analytics

--- Tsugi/UI/Output.php
+++ Tsugi/UI/Output.php
@@ -155,6 +155,18 @@ class Output {
 
     function doAnalytics() {
         global $CFG;
+        if ( $CFG->universal_analytics ) { ?>
+            <script>
+              (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+              (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+              })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ 
+              ga('create', '<?= $CFG->universal_analytics ?>', 'auto');
+              ga('send', 'pageview');
+ 
+            </script>
+	<?php }
         if ( $CFG->analytics_key ) { ?>
     <script type="text/javascript">
       var _gaq = _gaq || [];
