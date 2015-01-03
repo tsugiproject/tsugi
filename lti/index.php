a41d9885fc69d672dba92de12e5129aeaba1d87c
Charles Severance <csev@umich.edu>
2015-01-03 17:23:42 -0500

    Tweakage

--- lti/index.php
+++ lti/index.php
@@ -3,8 +3,8 @@ require_once("../config.php");
 
 $local_path = route_get_local_path(__DIR__);
 
-if ( $local_path == "content_item.xml" ) {
-    require_once("content_item_xml.php");
+if ( $local_path == "content-item.xml" ) {
+    require_once("content-item-xml.php");
     return;
 }
 ?>
