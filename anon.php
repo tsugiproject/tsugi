e4b56a5725faa92e0d916679ea2d6085bfa16424
Charles Severance <csev@umich.edu>
2015-02-15 13:07:32 -0500

    Wipe out the sha256 columns just in case someone wants to guess and verify

--- anon.php
+++ anon.php
@@ -11,12 +11,14 @@ DELETE FROM t_sample_blob;
 DELETE FROM t_sample_chat;
 DELETE FROM t_profile;
 
-UPDATE t_lti_key SET key_key=concat("key_",key_id), secret=concat("secret_",key_id),new_secret=null, settings=null, user_id=null, consumer_profile=null, new_consumer_profile=null;
-UPDATE t_lti_context SET title=concat("Context Title ",context_id), context_key=concat("context_key_",context_id), settings_url=NULL, settings=NULL, json=NULL;
-UPDATE t_lti_link SET title=concat("Link Title ",link_id), link_key=concat("link_key_",link_id), settings_url=NULL, settings=NULL,json=NULL;
+-- Note that SHA2('str', 256) does not work on all MySql installs
+
+UPDATE t_lti_key SET key_key=concat("key_",key_id), key_sha256=MD5(concat("key_",key_id)), secret=concat("secret_",key_id),new_secret=null, settings=null, user_id=null, consumer_profile=null, new_consumer_profile=null;
+UPDATE t_lti_context SET title=concat("Context Title ",context_id), context_key=concat("context_key_",context_id), context_sha256=MD5(concat("key_",context_id)), settings_url=NULL, settings=NULL, json=NULL;
+UPDATE t_lti_link SET title=concat("Link Title ",link_id), link_key=concat("link_key_",link_id), link_sha256=MD5(concat("link_key_",link_id)), settings_url=NULL, settings=NULL,json=NULL;
 UPDATE t_lti_result SET sourcedid=concat("sourcedid_",result_id), grade=RAND(),server_grade=NULL, json=NULL;
-UPDATE t_lti_service SET service_key=concat("http://www.example.com/service_",service_id), json=NULL;
-UPDATE t_lti_user SET user_key=concat("user_key_",user_id), displayname=concat("Displayname ", user_id),email=concat("user",user_id,"@example.com"),created_at=NOW(), updated_at=NOW(), login_at=NOW(),json=NULL;
+UPDATE t_lti_service SET service_key=concat("http://www.example.com/service_",service_id), service_sha256=MD5(concat("service_id_",service_id)), json=NULL;
+UPDATE t_lti_user SET user_key=concat("user_key_",user_id), user_sha256=MD5(concat("user_key_",user_id)), displayname=concat("Displayname ", user_id),email=concat("user",user_id,"@example.com"),created_at=NOW(), updated_at=NOW(), login_at=NOW(),json=NULL;
 UPDATE t_peer_assn SET json=NULL;
 UPDATE t_peer_flag SET note=concat("Flag Note ",flag_id),response=NULL;
 UPDATE t_peer_grade SET note=concat("Grade Note ",grade_id), points=FLOOR(RAND()*11);
