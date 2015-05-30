9007f386ae7de41a993c47cbff98e92c1fc37ca6
Charles Severance <csev@umich.edu>
2015-05-30 18:50:43 -0400

    Add support for the missing string functions.

--- mod/pythonauto/index.php
+++ mod/pythonauto/index.php
@@ -416,6 +416,40 @@ function outf(text) {
         return false;
     }
 
+// Deal with missing IE String functions
+// http://stackoverflow.com/questions/2308134/trim-in-javascript-not-working-in-ie
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/TrimLeft
+
+if(typeof String.prototype.trim !== 'function') {
+    String.prototype.trim = function() {
+        return this.replace(/^\s+|\s+$/g, '');
+    }
+}
+
+if(typeof String.prototype.ltrim !== 'function') {
+    String.prototype.ltrim = function() {
+        return this.replace(/^\s+/,"");
+    }
+}
+
+if(typeof String.prototype.trimLeft !== 'function') {
+    String.prototype.trimLeft = function() {
+        return this.replace(/^\s+/,"");
+    }
+}
+
+if(typeof String.prototype.rtrim !== 'function') {
+    String.prototype.rtrim = function() {
+        return this.replace(/\s+$/,"");
+    }
+}
+
+if(typeof String.prototype.trimRight !== 'function') {
+    String.prototype.trimRight = function() {
+        return this.replace(/\s+$/,"");
+    }
+}
+
 </script>
 <style>
 pre {
