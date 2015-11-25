52cb9b64b18528068e9591cea058033d420cb3eb
Charles Severance <csev@umich.edu>
2015-11-25 09:32:38 -0500

    Tweak

--- mod/python-data/data/index.php
+++ mod/python-data/data/index.php
@@ -273,7 +273,8 @@ if ( strlen($local_path) > 0 ) {
 This application has a number of test data sources for
 <a href="http://www.pythonlearn.com/" target="_blank">
 Python for Informatics: Exploring Information</a> written by
-<a href="http://www.twitter.com/drchuck" target="_blank">@DrChuck</a>.
+<a href="http://www.twitter.com/drchuck" target="_blank">@DrChuck</a> / 
+<a href="http://www.dr-chuck.com/" target="_blank">www.dr-chuck.com</a>.
 <ul>
 <li><a href="geojson" target="_blank">A subset of data from the Google Geo Coding API</a></li>
 <li><a href="regex_sum_42.txt" target=_"blank">Some data in a text file to be summed</a></li>
