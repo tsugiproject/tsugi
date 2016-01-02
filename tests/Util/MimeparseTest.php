1c7c76d4e8ff6c7fbfcef92d67fdf5ecf6b48fd4
Charles Severance <csev@umich.edu>
2016-01-02 13:49:31 -0500

    Some work on the unit tests.

--- tests/Util/MimeparseTest.php
+++ tests/Util/MimeparseTest.php
@@ -1,5 +1,10 @@
 <?php
 
+use \Tsugi\Util\Mimeparse;
+
+require_once("src/Util/Mimeparse.php");
+
+
 /**
  * Note from Chuck: This was downloaded from 
  *
@@ -13,92 +18,85 @@
 
 // Unit tests //////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+class MimeparseTest extends PHPUnit_Framework_TestCase
+{
 
-$m = new Mimeparse;
-if ($m->parse_media_range("application/xml;q=1") === array(0 => "application", 1=> "xml", 2=> array("q" => "1") ))
-    echo "application/xml;q=1 - OK<br>";
-else 
-    echo "application/xml;q=1 - FAIL<br>";
-    
-if ($m->parse_media_range("application/xml") === array(0 => "application", 1=> "xml", 2=> array("q" => "1") ))
-    echo "application/xml - OK<br>";
-else 
-    echo "application/xml - FAIL<br>";
-    
-if ($m->parse_media_range("application/xml;q=") === array(0 => "application", 1=> "xml", 2=> array("q" => "1") ))
-    echo "application/xml;q= - OK<br>";
-else 
-    echo "application/xml;q= - FAIL<br>";
-    
+    public $m;
 
-if ($m->parse_media_range("application/xml ; q=1;b=other") === array(0 => "application", 1=> "xml", 2=> array("q" => "1", "b" => "other") ))
-    echo "application/xml ; q=1;b=other - OK<br>";
-else 
-    echo "application/xml ; q=1;b=other - FAIL<br>";
-    
-if ($m->parse_media_range("application/xml ; q=2;b=other") === array(0 => "application", 1=> "xml", 2=> array("q" => "1", "b" => "other") ))
-    echo "application/xml ; q=2;b=other - OK<br>";
-else 
-    echo "application/xml ; q=2;b=other - FAIL<br>";
-    
-/* Java URLConnection class sends an Accept header that includes a single "*" */
-if ($m->parse_media_range(" *; q=.2") === array(0 => "*", 1=> "*", 2=> array("q" => ".2") ))
-    echo " *; q=.2 - OK<br>";
-else 
-    echo " *; q=.2 - FAIL<br>";
-    
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    public function testGet() {
+        $this->m = new Mimeparse();
 
-$accept = "text/*;q=0.3, text/html;q=0.7, text/html;level=1, text/html;level=2;q=0.4, */*;q=0.5";
+        $this->assertEquals($this->m->parse_media_range("application/xml;q=1"), array(0 => "application", 1=> "xml", 2=> array("q" => "1")),
+            "application/xml;q=1");
 
-echo (1 == $m->quality("text/html;level=1", $accept) ? 'text/html;level=1 - OK<br>' : 'text/html;level=1 - FAIL<br>');
-echo (0.7 == $m->quality("text/html", $accept) ? 'text/html - OK<br>' : 'text/html - FAIL<br>');
-echo (0.3 == $m->quality("text/plain", $accept) ? 'text/plain - OK<br>' : 'text/plain - FAIL<br>');
-echo (0.5 == $m->quality("image/jpeg", $accept) ? 'image/jpeg - OK<br>' : 'image/jpeg - FAIL<br>');
-echo (0.4 == $m->quality("text/html;level=2", $accept) ? 'text/html;level=2 - OK<br>' : 'text/html;level=2 - FAIL<br>');
-echo (0.7 == $m->quality("text/html;level=3", $accept) ? 'text/html;level=3 - OK<br>' : 'text/html;level=3 - FAIL<br>');
- 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        $this->assertEquals($this->m->parse_media_range("application/xml"), array(0 => "application", 1=> "xml", 2=> array("q" => "1")),
+            "application/xml");
 
-$supported_mime_types = array("application/xbel+xml", "application/xml");
+        $this->assertEquals($this->m->parse_media_range("application/xml;q="), array(0 => "application", 1=> "xml", 2=> array("q" => "1")),
+            "application/xml;q=");
 
-# direct match
-assert_best_match ("application/xbel+xml", "application/xbel+xml");
-# direct match with a q parameter
-assert_best_match ("application/xbel+xml", "application/xbel+xml; q=1");
-# direct match of our second choice with a q parameter
-assert_best_match ("application/xml", "application/xml; q=1");
-# match using a subtype wildcard
-assert_best_match ("application/xml", "application/*; q=1");
-# match using a type wildcard
-assert_best_match ("application/xml", "* / *");
+        $this->assertEquals($this->m->parse_media_range("application/xml ; q=1;b=other"), array(0 => "application", 1=> "xml", 2=> array("q" => "1", "b" => "other")),
+            "application/xml ; q=1;b=other");
 
-$supported_mime_types = array( "application/xbel+xml", "text/xml" );
-# match using a type versus a lower weighted subtype
-assert_best_match ("text/xml", "text/ *;q=0.5,* / *;q=0.1");
-# fail to match anything
-assert_best_match (null, "text/html,application/atom+xml; q=0.9" );
-# common AJAX scenario
-$supported_mime_types = array( "application/json", "text/html" );
-assert_best_match("application/json", "application/json, text/javascript, */*");
-# verify fitness sorting
-$supported_mime_types = array( "application/json", "text/html" );
-assert_best_match("application/json", "application/json, text/html;q=0.9");
+        $this->assertEquals($this->m->parse_media_range("application/xml ; q=2;b=other"), array(0 => "application", 1=> "xml", 2=> array("q" => "1", "b" => "other")),
+            "application/xml ; q=2;b=other");
+
+        /* Java URLConnection class sends an Accept header that includes a single "*" */
+        $this->assertEquals($this->m->parse_media_range(" *; q=.2"), array(0 => "*", 1=> "*", 2=> array("q" => ".2") ),
+            " *; q=.2");
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        $accept = "text/*;q=0.3, text/html;q=0.7, text/html;level=1, text/html;level=2;q=0.4, */*;q=0.5";
+
+        $this->assertEquals (1, $this->m->quality("text/html;level=1", $accept), 'text/html;level=1');
+        $this->assertEquals (0.7, $this->m->quality("text/html", $accept), 'text/html');
+        $this->assertEquals (0.3, $this->m->quality("text/plain", $accept), 'text/plain');
+        $this->assertEquals (0.5, $this->m->quality("image/jpeg", $accept), 'image/jpeg');
+        $this->assertEquals (0.4, $this->m->quality("text/html;level=2", $accept), 'text/html;level=2');
+        $this->assertEquals (0.7, $this->m->quality("text/html;level=3", $accept), 'text/html;level=3');
+
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        global $supported_mime_types;
+        $supported_mime_types = array("application/xbel+xml", "application/xml");
+
+        # direct match
+        $this->assert_best_match ("application/xbel+xml", "application/xbel+xml");
+        # direct match with a q parameter
+        $this->assert_best_match ("application/xbel+xml", "application/xbel+xml; q=1");
+        # direct match of our second choice with a q parameter
+        $this->assert_best_match ("application/xml", "application/xml; q=1");
+        # match using a subtype wildcard
+        $this->assert_best_match ("application/xml", "application/*; q=1");
+        # match using a type wildcard
+        $this->assert_best_match ("application/xml", "* / *");
+
+        $supported_mime_types = array( "application/xbel+xml", "text/xml" );
+        # match using a type versus a lower weighted subtype
+        $this->assert_best_match ("text/xml", "text/ *;q=0.5,* / *;q=0.1");
+        # fail to match anything
+        $this->assert_best_match (null, "text/html,application/atom+xml; q=0.9" );
+        # common AJAX scenario
+        $supported_mime_types = array( "application/json", "text/html" );
+        $this->assert_best_match("application/json", "application/json, text/javascript, */*");
+        # verify fitness sorting
+        $supported_mime_types = array( "application/json", "text/html" );
+        $this->assert_best_match("application/json", "application/json, text/html;q=0.9");
 
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-$supported_mime_types = array('image/*', 'application/xml');
-# match using a type wildcard
-assert_best_match ('image/*', 'image/png');
-# match using a wildcard for both requested and supported
-assert_best_match ('image/*', 'image/*');
+        /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        $supported_mime_types = array('image/*', 'application/xml');
+        # match using a type wildcard
+        $this->assert_best_match ('image/*', 'image/png');
+        # match using a wildcard for both requested and supported
+        $this->assert_best_match ('image/*', 'image/*');
+    }
 
-function assert_best_match($expected, $header) {
-    global $m, $supported_mime_types;
 
-    if ($expected == $m->best_match($supported_mime_types, $header)) 
-        echo "$header - OK<br>";
-    else 
-        echo "$header - FAIL<br>";
+    function assert_best_match($expected, $header) {
+        global $supported_mime_types;
+        $this->assertEquals($expected,$this->m->best_match($supported_mime_types, $header));
+    }
+
 }
-
--- tests/Util/NetTest.php
+++ tests/Util/NetTest.php
@@ -1,10 +1,10 @@
 <?php
 
-require "Net.php";
+require "src/Util/Net.php";
 
 use \Tsugi\Util\Net;
 
-class TestTest extends PHPUnit_Framework_TestCase
+class NetTest extends PHPUnit_Framework_TestCase
 {
     public function testGet() {
         $stuff = Net::doGet("http://www.dr-chuck.com/page1.htm");
