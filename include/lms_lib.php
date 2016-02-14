e43bc3caed0bb16dabccf5b19deb4732002249f5
Charles Severance <csev@umich.edu>
2016-02-14 08:21:03 -0500

    Remove admin-only code

--- include/lms_lib.php
+++ include/lms_lib.php
@@ -6,29 +6,6 @@ use \Tsugi\Core\Cache;
 use \Tsugi\Core\LTIX;
 use \Tsugi\Crypt\SecureCookie;
 
-// TODO: deal with headers sent...
-function requireLogin() {
-    global $CFG, $OUTPUT;
-    if ( ! isset($_SESSION['user_id']) ) {
-        $_SESSION['error'] = 'Login required';
-        $OUTPUT->doRedirect($CFG->wwwroot.'/login.php') ;
-        exit();
-    }
-}
-
-function isAdmin() {
-    return isset( $_SESSION['admin']) && $_SESSION['admin'] == 'yes';
-}
-
-function requireAdmin() {
-    global $CFG, $OUTPUT;
-    if ( $_SESSION['admin'] != 'yes' ) {
-        $_SESSION['error'] = 'Login required';
-        $OUTPUT->doRedirect($CFG->wwwroot.'/login.php') ;
-        exit();
-    }
-}
-
 function lmsDie($message=false) {
     global $CFG, $DEBUG_STRING;
     if($message !== false) {
@@ -57,56 +34,6 @@ function maxUpload() {
     $memory_limit = (int)(ini_get('memory_limit'));
     $upload_mb = min($maxUpload, $max_post, $memory_limit);
     return $upload_mb;
-}
-
-function findTools($dir, &$retval, $filename="index.php") {
-    if ( is_dir($dir) ) {
-        if ($dh = opendir($dir)) {
-            while (($sub = readdir($dh)) !== false) {
-                if ( strpos($sub, ".") === 0 ) continue;
-                $path = $dir . '/' . $sub;
-                if ( ! is_dir($path) ) continue;
-                if ( $sh = opendir($path)) {
-                    while (($file = readdir($sh)) !== false) {
-                        if ( $file == $filename ) {
-                            $retval[] = $path  ."/" . $file;
-                            break;
-                        }
-                    }
-                    closedir($sh);
-                }
-            }
-            closedir($dh);
-        }
-    }
-}
-
-function findFiles($filename="index.php", $reldir=false) {
-    global $CFG;
-    $files = array();
-    foreach ( $CFG->tool_folders as $dir ) {
-        if ( $reldir !== false ) $dir = $reldir . $dir;
-        if ( is_dir($dir) ) {
-            if ($dh = opendir($dir)) {
-                while (($sub = readdir($dh)) !== false) {
-                    if ( strpos($sub, ".") === 0 ) continue;
-                    $path = $dir . '/' . $sub;
-                    if ( ! is_dir($path) ) continue;
-                    if ( $sh = opendir($path)) {
-                        while (($file = readdir($sh)) !== false) {
-                            if ( $file == $filename ) {
-                                $files[] = $path  ."/" . $file;
-                                break;
-                            }
-                        }
-                        closedir($sh);
-                    }
-                }
-                closedir($dh);
-            }
-        }
-    }
-    return $files;
 }
 
 // Looks up a result for a potentially different user_id so we make
