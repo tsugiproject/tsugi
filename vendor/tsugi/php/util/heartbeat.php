4af90faac4511efbf146077a5a08fedf7e8cc5af
Charles Severance <csev@umich.edu>
2016-02-19 11:04:50 -0500

    Refactor heartbeat.php to use a class.

--- vendor/tsugi/php/util/heartbeat.php
+++ vendor/tsugi/php/util/heartbeat.php
@@ -8,34 +8,5 @@ if ( isset($_GET[session_name()]) ) {
 }
 
 require_once "../../../../config.php";
-require_once $CFG->vendorinclude . "/lms_lib.php";
 
-use \Tsugi\UI\Output;
-
-Output::headerJson();
-
-session_start();
-
-// See how long since the last update of the activity time
-$seconds = 0;
-$now = time();
-if ( isset($_SESSION['LAST_ACTIVITY']) ) {
-    $seconds = $now - $_SESSION['LAST_ACTIVITY'];
-}
-$_SESSION['LAST_ACTIVITY'] = $now; // update last activity time stamp
-
-// Count the successive heartbeats without a request/response cycle
-if ( ! isset($_SESSION['HEARTBEAT_COUNT']) ) $_SESSION['HEARTBEAT_COUNT'] = 0;
-$count = $_SESSION['HEARTBEAT_COUNT']++;
-
-if ( $count > 10 && ( $count % 100 ) == 0 ) {
-    error_log("Heartbeat.php ".session_id().' '.$count);
-}
-
-$retval = array("success" => true, "seconds" => $seconds,
-        "now" => $now, "count" => $count, "cookie" => $cookie,
-        "id" => session_id());
-$retval['lti'] = isset($_SESSION['lti']);
-// $retval['lti'] = false;
-$retval['sessionlifetime'] = $CFG->sessionlifetime;
-echo(json_encode($retval));
+\Tsugi\UI\Output::handleHeartBeat($cookie);
