4ff470abdffab7535e9386d4b9c5b3d8f28529e8
Charles Severance <csev@umich.edu>
2016-04-15 11:17:50 +0300

    remove samples

--- samples/caliper/register.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$REGISTER_LTI2 = array(
-"name" => "Caliper Unit Test Tool",
-"FontAwesome" => "fa-cubes",
-"short_name" => "Caliper Test",
-"description" => "This tool tests the pre-production Caliper API from Canvas."
-);
-
--- samples/settings/register.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$REGISTER_LTI2 = array(
-"name" => "Settings Unit Test Tool",
-"FontAwesome" => "fa-database",
-"short_name" => "Settings Test",
-"description" => "This tool allows you to exercise the LTI 2.x JSON Settings API."
-);
-
