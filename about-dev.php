8cf67c3b7c02a1768e31fbbbf18a0986cebc45c7
Charles Severance <csev@umich.edu>
2016-08-14 23:43:05 -0400

    Cleanup

--- about-dev.php
+++ about-dev.php
@@ -14,17 +14,11 @@ $OUTPUT->bodyStart();
 <p>
 This developer screen allows you to quickly test TSUGI applications.
 It allows you to change any LTI lauch parameter and switch
-between a set of user account data.  This feature is intended
-to test instances running on a developer desktop (i.e. not suitable
-for production) and so to enable this, you need to have the
-<pre>
-$CFG->DEVELOPER flag set to true
-</pre>
-in the config.php file for this application.
+between a sets of fake user account data.  
 </p>
 <?php if ( $CFG->DEVELOPER ) { ?>
 <a class="btn btn-primary" href="dev.php" role="button">Back</a>
 <?php } else { ?>
-<a class="btn btn-primary" href="index.php" role="button">Back</a>
+<a class="btn btn-primary" href="<?= $CFG->apphome ?>/index.php" role="button">Back</a>
 <?php } 
 $OUTPUT->footer();
