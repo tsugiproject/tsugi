f02be36be4d15b462587f0ed64f4d4cfdfc39d30
Charles Severance <csev@umich.edu>
2016-08-23 07:53:26 -0400

    Improve badges, fix Lessons object

--- badges.php
+++ badges.php
@@ -1,7 +1,12 @@
 <?php
+
+use \Tsugi\UI\Lessons;
+
 // In the top frame, we use cookies for session.
 define('COOKIE_SESSION', true);
 require_once("config.php");
+
+$l = new Lessons('../lessons.json');
 
 header('Content-Type: text/html; charset=utf-8');
 session_start();
@@ -10,7 +15,15 @@ $OUTPUT->header();
 $OUTPUT->bodyStart();
 $OUTPUT->topNav();
 
-echo("<p>This feature to award Open Badges is in progress. You can work on the course and when the badge feature is complete all of your progress will be credited towards your badge(s).</p>\n");
+echo("<p>This feature to award Open Badges is in progress. You can work on the course and when the badge feature is complete all of your progress will be retroactively credited towards your badge(s).</p>\n");
+if ( isset( $l->lessons->badges) ) {
+    echo("<p>The following badges will be awarded for this course when the badge feature is completed</p>\n");
+    echo("<ul>\n");
+    foreach($l->lessons->badges as $badge) {
+        echo('<li>'.$badge->title.'</li>'."\n");
+    }
+    echo("</ul>\n");
+}
 echo('<p><a href="https://www.imsglobal.org/initiative/enabling-better-digital-credentialing" target="blank">IMS Digital Credentials initiative</a></p>'."\n");
 
 $OUTPUT->footer();
