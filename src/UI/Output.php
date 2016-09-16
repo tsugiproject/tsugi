dadd93bcb459afb6a6030fe18e5d0c0d50328f7d
Charles Severance <csev@umich.edu>
2016-09-15 21:06:36 -0400

    Remove extra bits

--- src/UI/Output.php
+++ src/UI/Output.php
@@ -212,7 +212,6 @@ function googleTranslateElementInit() {
     }, "google_translate_element");
 }
 </script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
-';
 <?php
 
         }
