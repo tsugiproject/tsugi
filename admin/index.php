54a1f991cf99eab9af0c5226af186fb193fd2367
Charles Severance <csev@umich.edu>
2017-01-01 12:18:07 -0500

    Put database and nonces in modals

--- admin/index.php
+++ admin/index.php
@@ -14,10 +14,22 @@ $OUTPUT->bodyStart();
 $OUTPUT->topNav();
 require_once("sanity-db.php");
 ?>
+<div id="iframe-dialog" title="Read Only Dialog" style="display: none;">
+   <iframe name="iframe-frame" style="height:400px" id="iframe-frame" 
+    src="<?= $OUTPUT->getSpinnerUrl() ?>"></iframe>
+</div>
 <h1>Welcome Adminstrator</h1>
 <ul>
-<li><a href="upgrade.php" target="_new">Upgrade Database</a></li>
-<li><a href="nonce.php" target="_new">Check Nonces</a></li>
+<li>
+  <a href="upgrade.php" title="Upgrade Database" target="iframe-frame"
+  onclick="showModalIframe(this.title, 'iframe-dialog', 'iframe-frame', _TSUGI.spinnerUrl);" >
+  Ugrade Database 
+  </a>
+<li>
+  <a href="nonce.php" title="Check Nonces" target="iframe-frame"
+  onclick="showModalIframe(this.title, 'iframe-dialog', 'iframe-frame', _TSUGI.spinnerUrl);" >
+  Check Nonces 
+  </a>
 <li><a href="context/index.php">View Contexts</a></li>
 <?php if ( $CFG->providekeys ) { ?>
 <li><a href="key/index.php">Manage Access Keys</a></li>
