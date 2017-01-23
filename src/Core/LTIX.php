3f7fb1cfdbc16bd95c3120d271550a3f63723f49
Charles Severance <csev@umich.edu>
2017-01-23 01:14:31 -0500

    Small improvements

--- src/Core/LTIX.php
+++ src/Core/LTIX.php
@@ -255,7 +255,8 @@ class LTIX {
 
         // We have a Authorization-header with OAuth data. Parse the header
         // and add those overriding any duplicates from GET or POST
-        if (@substr($request_headers['Authorization'], 0, 6) == "OAuth ") {
+        if (isset($request_headers['Authorization']) &&
+            substr($request_headers['Authorization'], 0, 6) == "OAuth ") {
             $header_parameters = OAuthUtil::split_header(
                 $request_headers['Authorization']
             );
