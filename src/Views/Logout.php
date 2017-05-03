248ae6a06ce0c0313458c450a0f2a9c1ac1e1fe1
Charles Severance <csev@umich.edu>
2017-05-02 23:34:13 -0400

    Move logout to a view

---
+++ src/Views/Logout.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tsugi\Views;
+
+use Silex\Application;
+
+use \Tsugi\Crypt\SecureCookie;
+
+class Logout {
+
+    const ROUTE = '/logout';
+
+    public static function routes(Application $app, $prefix=self::ROUTE) {
+        $app->get($prefix, function () {
+            global $CFG;
+            session_unset();
+            SecureCookie::delete();
+            header('Location: '.$CFG->apphome.'/index.php');
+        });
+    }
+}
