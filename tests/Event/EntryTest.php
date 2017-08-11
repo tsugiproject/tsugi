f098bfcc3169ccc80e49b54b9361fe146955fb8d
Charles Severance <csev@umich.edu>
2017-08-11 15:19:55 -0400

    Initial commit of places to work on analytics

---
+++ tests/Event/EntryTest.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once "src/Event/Entry.php";
+
+use \Tsugi\Util\U;
+
+class EntryTest extends PHPUnit_Framework_TestCase
+{
+    public function testY() {
+echo("\nYADA\n");
+    }
+
+}
