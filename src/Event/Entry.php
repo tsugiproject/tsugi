9065979e9e6b03c26b85bde901242bdda053882c
Charles Severance <csev@umich.edu>
2017-08-12 13:09:39 -0400

    Progress.

--- src/Event/Entry.php
+++ src/Event/Entry.php
@@ -2,6 +2,8 @@
 
 namespace Tsugi\Event;
 
+use \Tsugi\Util\U;
+
 /**
  * A adaptive, multi-scale compressed time series of event counter buckets
  *
@@ -10,33 +12,107 @@ namespace Tsugi\Event;
 class Entry {
 
     /**
-     * The earliest time for the first bucket
+     * The earliest time for the first bucket - default first entry
      */
-    public $timestart = 0;
+    public $timestart = null;
 
     /**
      * The scale in seconds
      */
-    public $scale = 0;
+    public $scale = 15*60;
 
     /**
-     * The length of the arrays
+     * The max length (bytes) of the field post serialization
      */
-    public $size = 0;
+    public $maxlen = 0;
 
     /**
-     * The offsets (0-65K)
+     * The total number of clicks
      */
-    public $offsets = null;
+    public $total = 0;
 
     /**
-     * The offsets (0-65K)
+     * The buckets key->value
      */
-    public $buckets = null;
+    public $buckets = array();
 
-    public function __construct($start=null, $size=256, $scale=15*60)
-    {
-        $this->size = $size;
+    public function __construct($timestart=null, $scale=15*60, $maxlen=1024) {
+        $this->timestart = (int) ($timestart / $scale);
+        $this->scale = $scale;
+        $this->maxlen = $maxlen;
+    }
+
+    /**
+     * Record a click...
+     */
+    public function click($time=null) {
+        $this->total++;
+        if ( ! $this->timestart ) {
+            $this->timestart = (int) ( time() / $this->scale );
+        }
+        if ( ! $time ) $time = time();
+        $time = (int) ($time / $this->scale);
+        $delta = $time - $this->timestart;
+        if ( $delta < 0 ) $delta = 0;  // 0 is OK
+        if ( isset($this->buckets[$delta]) ) {
+            $this->buckets[$delta]++;
+        } else {
+            $this->buckets[$delta] = 1;
+        }
+    }
+
+    /**
+     * Reconstruct to the actual times
+     */
+    public function reconstruct() {
+        $retval = array();
+        foreach($this->buckets as $k => $v) {
+            $t = ($this->timestart + $k) * $this->scale;
+            $retval[$t] = $v;
+        }
+        return $retval;
+    }
+
+    /**
+     * Double the Scale - Return *copy of* new buckets
+     */
+    public function reScale($factor=2) {
+        $newbuckets = array();
+        $newscale = $this->scale * $factor;
+        $newstart = (int) ($this->timestart / $factor);
+        foreach($this->buckets as $k => $v) {
+            $oldtime = ($this->timestart + $k) * $this->scale;
+            $newposition = (int) ($oldtime / $newscale);
+            $delta = $newposition - $newstart;
+            if ( isset($newbuckets[$delta]) ) {
+                $newbuckets[$delta] += $v;
+            } else {
+                $newbuckets[$delta] = $v;
+            }
+        }
+        return $newbuckets;
+    }
+
+    /**
+     *  Serialize to a key=value pair
+     */
+    public function serialize($maxlength=null, $compress=false) {
+        if ( ! $maxlength ) $maxlength = $this->maxlen;
+        $retval = $this->scale . ':' .$this->timestart . ':' . U::array_Integer_Serialize($this->buckets);
+        if ( strlen($retval) <= $maxlength ) return $retval;
+        // TODO: Compress later
+        // Make copies so we can try various strategies
+        $scale = $this->scale;
+        $buckets = $this->buckets;
+        // Strategy 1 - Double Scale
+        foreach(array(2,4) as $factor ) {
+            $newbuckets = $this->reScale($factor);
+            $newscale = $this->scale * $factor;
+            $newstart = (int) ($this->timestart / $factor);
+            $retval = $newscale . ':' .$newstart . ':' . U::array_Integer_Serialize($newbuckets);
+            if ( strlen($retval) <= $maxlength ) return $retval;
+        }
+die('BAD');
     }
 
 }
--- tests/Event/EntryTest.php
+++ tests/Event/EntryTest.php
@@ -2,13 +2,47 @@
 
 require_once "src/Event/Entry.php";
 
+use \Tsugi\Util\U;
 use \Tsugi\Event\Entry;
 
 class EntryTest extends PHPUnit_Framework_TestCase
 {
     public function testY() {
-        echo("\ntestY\n");
-        $ent = new Entry();
+        $x = 1502553699;
+        $ent = new Entry($x);
+        $ent->click($x);
+
+        $this->assertEquals($ent->serialize(),'900:1669504:0=1');
+        $this->assertEquals($ent->reconstruct(),array(1502553600 => 1));
+
+        // Fil up some more
+        for($month=0; $month<=2; $month++) {
+        for($day=0;$day<=2;$day++) {
+        for($sec=0; $sec<2000;$sec++) { 
+            $now = $month*60*60*24*30 + $day*60*60*24 + $sec;
+            $ent->click($x+$now);
+        }}}
+
+        // Look at the actual buckets
+        $this->assertEquals(U::array_Integer_Serialize($ent->buckets),"0=802,1=900,2=299,96=801,97=900,98=299,192=801,193=900,194=299,2880=801,2881=900,2882=299,2976=801,2977=900,2978=299,3072=801,3073=900,3074=299,5760=801,5761=900,5762=299,5856=801,5857=900,5858=299,5952=801,5953=900,5954=299");
+
+        // Call the serializer w/o the need to compress
+        $ser =$ent->serialize(1000);
+        $this->assertEquals($ser,"900:1669504:0=802,1=900,2=299,96=801,97=900,98=299,192=801,193=900,194=299,2880=801,2881=900,2882=299,2976=801,2977=900,2978=299,3072=801,3073=900,3074=299,5760=801,5761=900,5762=299,5856=801,5857=900,5858=299,5952=801,5953=900,5954=299");
+
+        // Test rescaling...
+        $newbuck = $ent->reScale();
+        $this->assertEquals(U::array_Integer_Serialize($newbuck),"0=1702,1=299,48=1701,49=299,96=1701,97=299,1440=1701,1441=299,1488=1701,1489=299,1536=1701,1537=299,2880=1701,2881=299,2928=1701,2929=299,2976=1701,2977=299");
+
+        $smaller = (int) (strlen($ser) * 0.75);
+        $compress = false;
+        $ser2 = $ent->serialize($smaller, $compress);
+        $this->assertEquals($ser2,"1800:834752:0=1702,1=299,48=1701,49=299,96=1701,97=299,1440=1701,1441=299,1488=1701,1489=299,1536=1701,1537=299,2880=1701,2881=299,2928=1701,2929=299,2976=1701,2977=299");
+        // var_dump($ser2);
+
+        $smaller = (int) (strlen($ser) * 0.5);
+        $ser3 = $ent->serialize($smaller, $compress);
+        $this->assertEquals($ser3,"3600:417376:0=2001,24=2000,48=2000,720=2000,744=2000,768=2000,1440=2000,1464=2000,1488=2000");
     }
 
 }
