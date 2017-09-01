643c8df61f858a7bb8ac45442ccaca74e3c93382
Charles Severance <csev@umich.edu>
2017-09-01 11:14:54 -0400

    Tweaks

--- admin/key/using.php
+++ admin/key/using.php
@@ -42,6 +42,7 @@ $OUTPUT->flashMessages();
   <li><a href="#sakai" data-toggle="tab" aria-expanded="false">Sakai</a></li>
   <li><a href="#canvas" data-toggle="tab" aria-expanded="false">Canvas</a></li>
   <li><a href="#moodle" data-toggle="tab" aria-expanded="false">Moodle</a></li>
+  <li><a href="#bb" data-toggle="tab" aria-expanded="false">Blackboard</a></li>
   <li><a href="#lti2" data-toggle="tab" aria-expanded="false">LTI 2.x</a></li>
 </ul>
 <div id="myTabContent" class="tab-content" style="margin-top:10px;">
@@ -89,13 +90,33 @@ This content will be available in the Modules, Pages, Assignments, and Import
 within Canvas under "external tools".
 </div>
 <div class="tab-pane fade" id="moodle">
-Moodle 3.4 and later supports the IMS Content Item standard so you can install
+<p>
+You can import all the content (including autograders) from this site into your Moodle
+course by downloading it as an 
+<a href="<?= $CFG->wwwroot ?>/cc/export">Common Cartridge</a> and then using
+the Restore feature to upload the content into your Moodle course.   Once you
+install your LTI key and secret in the course, the LTI links to the autograder
+should start to function.
+</p>
+<p>
+Moodle 3.4 and later also supports the IMS Content Item standard so you can install
 this site as an "App Store" / "Learning Object Repository" using this url:
 <pre>
 <?= $CFG->wwwroot ?>/lti/store/
 </pre>
 Make sure to find and check the "Supports Content Item" option when installing 
 this URL.
+</p>
+</div>
+<div class="tab-pane fade" id="bb">
+<p>
+You can import all the content from (including autograders) this site into your Blackboard
+course by downloading it as an 
+<a href="<?= $CFG->wwwroot ?>/cc/export">Common Cartridge</a> and then using
+the Import feature to upload the content into your course.   Once you
+install your LTI key and secret in the course, the LTI links to the autograder
+should start to function.
+</p>
 </div>
 <div class="tab-pane fade" id="lti2">
 If your LMS supports LTI 2.x and you have received an LTI 2 key for this service,
