52256ec7b12ef52c13ccc5078399ed03848ea5c7
Charles R. Severance <csev@umich.edu>
2017-10-18 11:17:37 -0400

    fix blob model

--- admin/blob/database.php
+++ admin/blob/database.php
@@ -17,8 +17,8 @@ array( "{$CFG->dbprefix}blob_file",
     content      LONGBLOB NULL,
 
     json         TEXT NULL,
-    created_at   DATETIME NOT NULL,
-    accessed_at  DATETIME NOT NULL,
+    created_at          TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    accessed_at          TIMESTAMP NOT NULL DEFAULT '1970-01-02 00:00:00',
 
     INDEX `{$CFG->dbprefix}blob_indx_1` USING HASH (`file_sha256`),
 
@@ -33,6 +33,33 @@ array( "{$CFG->dbprefix}blob_file",
 $DATABASE_UNINSTALL = "drop table if exists {$CFG->dbprefix}blob_file";
 
 // No upgrades yet
-$DATABASE_UPGRADE = function($oldversion) { return 1; };
+$DATABASE_UPGRADE = function($oldversion) { 
+    global $CFG, $PDOX;
+
+    if ( $oldversion < 201810151700 ) {
+        $sql= "UPDATE {$CFG->dbprefix}blob_file SET created_at='1970-01-02 00:00:00' WHERE created_at < '1970-01-02 00:00:00'";
+        echo("Upgrading: ".$sql."<br/>\n");
+        error_log("Upgrading: ".$sql);
+        $q = $PDOX->queryDie($sql);
+
+        $sql= "UPDATE {$CFG->dbprefix}blob_file SET accessed_at='1970-01-02 00:00:00' WHERE accessed_at < '1970-01-02 00:00:00'";
+        echo("Upgrading: ".$sql."<br/>\n");
+        error_log("Upgrading: ".$sql);
+        $q = $PDOX->queryDie($sql);
+
+        $sql= "ALTER TABLE {$CFG->dbprefix}blob_file MODIFY created_at TIMESTAMP NOT NULL DEFAULT '1970-01-02 00:00:00'";
+        echo("Upgrading: ".$sql."<br/>\n");
+        error_log("Upgrading: ".$sql);
+        $q = $PDOX->queryDie($sql);
+
+        $sql= "ALTER TABLE {$CFG->dbprefix}blob_file MODIFY accessed_at TIMESTAMP NOT NULL DEFAULT '1970-01-02 00:00:00'";
+        echo("Upgrading: ".$sql."<br/>\n");
+        error_log("Upgrading: ".$sql);
+        $q = $PDOX->queryDie($sql);
+    }
+
+    return 201710151700;
+
+};
 
 
