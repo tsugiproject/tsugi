7379a6a6e68238f4b33f0d73fc2cbd4fefc99797
Charles Severance <csev@umich.edu>
2017-11-27 17:17:02 -0500

    Walking down the launch path

--- gclass/launch.php
+++ gclass/launch.php
@@ -1,8 +1,18 @@
 <?php
 
-use \Tsugi\Util\U;
-
+// Only from within tsugi.php
 if ( ! isset($CFG) ) return;
+
+use \Tsugi\Util\U;
+use \Tsugi\Util\Net;
+use \Tsugi\UI\Lessons;
+use \Tsugi\Core\LTIX;
+use \Tsugi\Crypt\SecureCookie;
+use \Tsugi\Crypt\AesCtr;
+
+$PDOX = LTIX::getConnection();
+
+session_start();
 
 $parts = U::parse_rest_path();
 ?>
@@ -20,3 +30,10 @@ print_r($_POST);
 Get:
 <?php
 print_r($_GET);
+?>
+<hr/>
+Apache Request Headers:
+<?php
+$request_headers = apache_request_headers();
+print_r($request_headers);
+error_log(\Tsugi\UI\Output::safe_var_dump($request_headers));
