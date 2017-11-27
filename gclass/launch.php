c7283e01043740471ef226649da17b924f675ce8
Charles R. Severance <csev@umich.edu>
2017-11-27 17:35:44 -0500

    Send back preview

--- gclass/launch.php
+++ gclass/launch.php
@@ -1,7 +1,6 @@
 <?php
 
-// Only from within tsugi.php
-if ( ! isset($CFG) ) return;
+if ( ! isset($CFG) ) return; // Only from within tsugi.php
 
 use \Tsugi\Util\U;
 use \Tsugi\Util\Net;
@@ -9,6 +8,14 @@ use \Tsugi\UI\Lessons;
 use \Tsugi\Core\LTIX;
 use \Tsugi\Crypt\SecureCookie;
 use \Tsugi\Crypt\AesCtr;
+
+$request_headers = apache_request_headers();
+$agent = U::get($request_headers,'User-Agent');
+if ( $agent && stripos($agent,'Google Web Preview') !== false ) {
+    echo('<center><img src="'.$CFG->apphome.'/logo.png"></center>'."\n");
+    error_log("IMAGE THUMBNAIL!!");
+    return;
+}
 
 $PDOX = LTIX::getConnection();
 
@@ -34,6 +41,5 @@ print_r($_GET);
 <hr/>
 Apache Request Headers:
 <?php
-$request_headers = apache_request_headers();
 print_r($request_headers);
 error_log(\Tsugi\UI\Output::safe_var_dump($request_headers));
