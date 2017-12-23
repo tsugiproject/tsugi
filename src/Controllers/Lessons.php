8d81e8b4be467cf056a18c131728b75accc4cbc0
Charles Severance <csev@umich.edu>
2017-12-22 22:45:28 -0500

    Refactor how Lessons launches happen

--- src/Controllers/Lessons.php
+++ src/Controllers/Lessons.php
@@ -3,6 +3,8 @@
 namespace Koseu\Controllers;
 
 use Tsugi\Util\U;
+use Tsugi\Util\LTI;
+use Tsugi\Core\LTIX;
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -15,6 +17,7 @@ class Lessons {
         $app->get($prefix, 'Koseu\\Controllers\\Lessons::get');
         $app->get($prefix.'/', 'Koseu\\Controllers\\Lessons::get');
         $app->get($prefix.'/{anchor}', 'Koseu\\Controllers\\Lessons::get');
+        $app->get($prefix.'_launch/{anchor}', 'Koseu\\Controllers\\Lessons::launch');
     }
 
     public function get(Request $request, Application $app, $anchor=null)
@@ -45,5 +48,87 @@ class Lessons {
 
         return $app['twig']->render('@Koseu/Lessons.twig',$context);
 
+    }
+
+    public function launch(Request $request, Application $app, $anchor=null)
+    {
+        global $CFG;
+        $tsugi = $app['tsugi'];
+
+        if ( ! isset($CFG->lessons) ) {
+            die_with_error_log('Cannot find lessons.json ($CFG->lessons)');
+        }
+
+        /// Load the Lesson
+        $l = new \Tsugi\UI\Lessons($CFG->lessons);
+        if ( ! $l ) {
+            die_with_error_log('Canot load lessons.');
+        }
+
+        $module = $l->getModuleByRlid($anchor);
+        if ( ! $module ) {
+            die_with_error_log('Cannot find module resource link id');
+        }
+
+        $lti = $l->getLtiByRlid($anchor);
+        if ( ! $lti ) {
+            die_with_error_log('Cannot find lti resource link id');
+        }
+
+        $path = U::rest_path();
+
+        $resource_link_title = isset($lti->title) ? $lti->title : $module->title;
+        $key = isset($_SESSION['oauth_consumer_key']) ? $_SESSION['oauth_consumer_key'] : false;
+        $secret = false;
+        if ( isset($_SESSION['secret']) ) {
+            $secret = LTIX::decrypt_secret($_SESSION['secret']);
+        }
+
+        $resource_link_id = $lti->resource_link_id;
+        $parms = array(
+            'lti_message_type' => 'basic-lti-launch-request',
+            'resource_link_id' => $resource_link_id,
+            'resource_link_title' => $resource_link_title,
+            'tool_consumer_info_product_family_code' => 'tsugi',
+            'tool_consumer_info_version' => '1.1',
+            'context_id' => $_SESSION['context_key'],
+            'context_label' => $CFG->context_title,
+            'context_title' => $CFG->context_title,
+            'user_id' => $_SESSION['user_key'],
+            'lis_person_name_full' => $_SESSION['displayname'],
+            'lis_person_contact_email_primary' => $_SESSION['email'],
+            'roles' => 'Learner'
+        );
+        if ( isset($_SESSION['avatar']) ) $parms['user_image'] = $_SESSION['avatar'];
+
+        if ( isset($lti->custom) ) {
+            foreach($lti->custom as $custom) {
+                if ( isset($custom->value) ) {
+                    $parms['custom_'.$custom->key] = $custom->value;
+                }
+                if ( isset($custom->json) ) {
+                    $parms['custom_'.$custom->key] = json_encode($custom->json);
+                }
+            }
+        }
+
+        $return_url = $path->parent . '/' . str_replace('_launch', '', $path->controller) . '/' . $module->anchor;
+        $parms['launch_presentation_return_url'] = $return_url;
+
+        $sess_key = 'tsugi_top_nav_'.$CFG->wwwroot;
+        if ( isset($_SESSION[$sess_key]) ) {
+            // $parms['ext_tsugi_top_nav'] = $_SESSION[$sess_key];
+        }
+
+        $form_id = "tsugi_form_id_".bin2Hex(openssl_random_pseudo_bytes(4));
+        $parms['ext_lti_form_id'] = $form_id;
+
+        $endpoint = $lti->launch;
+        $parms = LTI::signParameters($parms, $endpoint, "POST", $key, $secret,
+            "Finish Launch", $CFG->product_instance_guid, $CFG->servicename);
+
+        $content = LTI::postLaunchHTML($parms, $endpoint, false /*debug */);
+        print($content);
+        return "";
     }
 }
