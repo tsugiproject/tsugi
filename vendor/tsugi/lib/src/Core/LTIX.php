77b315a6c8205f6ffb5a796ac00bdcf8acd3afc9
Charles Severance <csev@umich.edu>
2017-12-26 11:37:14 -0500

    Allow 12345 to be used outside of developer mode

--- vendor/tsugi/lib/src/Core/LTIX.php
+++ vendor/tsugi/lib/src/Core/LTIX.php
@@ -359,10 +359,6 @@ class LTIX {
             die();
         }
 
-        if ( $post['key'] == '12345' && ! $CFG->DEVELOPER) {
-            self::abort_with_error_log('You can only use key 12345 in developer mode');
-        }
-
         // We make up a Session ID Key because we don't want a new one
         // each time the same user launches the same link.
         if ( $session_object === null ) {
