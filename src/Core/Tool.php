4ef13006ddeab45cf422cd8f72e3d03648bd5f1d
Charles Severance <csev@umich.edu>
2017-12-27 09:14:50 -0500

    Moar progress on tool features.

--- src/Core/Tool.php
+++ src/Core/Tool.php
@@ -1,6 +1,10 @@
 <?php
 
-namespace Tsugi\Tool;
+namespace Tsugi\Core;
+
+use \Tsugi\Util\U;
+use \Tsugi\Util\PS;
+use \Tsugi\UI\Output;
 
 /**
  * Provide support for a Tsugi Tool
@@ -14,6 +18,19 @@ class Tool {
     }
 
     public function run() {
+        // Check for a few special cases
+        $rest_path = U::rest_path();
+        if ( file_exists('register.php') && $rest_path->controller == 'register.json') {
+            $reg = $this->loadRegistration();
+            if ( is_string($reg) ) {
+                OUTPUT::jsonError($reg);
+                return;
+            }
+            OUTPUT::headerJson();
+            echo(json_encode($reg,JSON_PRETTY_PRINT));
+            return;
+        }
+
         // Make PHP paths pretty .../install => install.php
         $router = new \Tsugi\Util\FileRouter();
         $file = $router->fileCheck();
@@ -32,5 +49,46 @@ class Tool {
         }
 
         $app->run();
+    }
+
+    public static function loadRegistration() {
+        global $CFG;
+        require_once('register.php');
+        $rest_path = U::rest_path();
+        $url = $CFG->wwwroot . $rest_path->parent;
+
+        if ( ! isset($REGISTER_LTI2) ) return "Error in register.php";
+        if ( ! is_array($REGISTER_LTI2) ) return "Error in register.php";
+
+        if ( isset($REGISTER_LTI2['name']) && isset($REGISTER_LTI2['short_name']) &&
+            isset($REGISTER_LTI2['description']) ) {
+            // We are happy
+        } else {
+            error_log("Missing required name, short_name, and description in ".$tool_folder);
+            return "Missing required name, short_name, and description in ".$tool_folder;
+        }
+
+        // Make an icon URL
+        $fa_icon = isset($REGISTER_LTI2['FontAwesome']) ? $REGISTER_LTI2['FontAwesome'] : false;
+        if ( $fa_icon !== false ) {
+            $REGISTER_LTI2['icon'] = $CFG->fontawesome.'/png/'.str_replace('fa-','',$fa_icon).'.png';
+        }
+        $REGISTER_LTI2['url'] = $url;
+
+        $screen_shots = U::get($REGISTER_LTI2, 'screen_shots');
+        if ( is_array($screen_shots) && count($screen_shots) > 0 ) {
+            $new = array();
+            foreach($screen_shots as $screen_shot ) {
+                $ps = PS::s($screen_shot);
+                if ( $ps->startsWith('http://') || $ps->startsWith('https://') ) {
+                    $new[] = $url;
+                } else {
+                    $new[] = $url . '/' . $screen_shot;
+                }
+            }
+            $REGISTER_LTI2['screen_shots'] = $new;
+        }
+
+        return $REGISTER_LTI2;
     }
 }
