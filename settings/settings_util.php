01df17d6e45c8187b51e597ebd49c80d8cdc8411
Charles R. Severance <csev@umich.edu>
2017-12-29 04:48:02 +0000

    Tweak the UI.

--- settings/settings_util.php
+++ settings/settings_util.php
@@ -20,7 +20,7 @@ function settings_status($key_count) {
     if ( ! U::get($_SESSION,'id') ) {
         return "<p><b>You must log in to use these tools in your learning management system or Google Classroom.</b></p>";
     } else if ( U::get($_SESSION,'gc_count') ) {
-        return "<p><b>You have access to ".U::get($_SESSION,'gc_count')." google classroom courses.  Use the icons below to install the tools in your classes.</b></p>";
+        return "<p><b>You have access to ".U::get($_SESSION,'gc_count')." Google Classroom courses.  Use the icons below to install the tools in your classes.</b></p>";
     } else if ( ! U::get($_SESSION,'gc_count') && $key_count < 1 && 
         ( isset($CFG->providekeys) || isset($CFG->google_classroom_secret) ) ) {
         $retval = "<p><b>You need to ";
