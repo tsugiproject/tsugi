ab7d47130753bd4fbcbce930c6f7ebf6150f905d
Charles R. Severance <csev@umich.edu>
2018-01-03 00:42:34 +0000

    Better login call.

--- gclass/launch.php
+++ gclass/launch.php
@@ -43,7 +43,7 @@ $context_sha256 = lti_sha256($context_key);
 
 if ( ! isset($_SESSION['id']) ) {
     $_SESSION['login_return'] = $path[0].'/'.$path[1].'/'.$path[2];
-    header('Location: '.$CFG->apphome.'/login');
+    header('Location: '.$CFG->wwwroot.'/login');
     return;
 }
 
