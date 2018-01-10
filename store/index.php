bb36ed3731e8b2fe90b1fc88e4f466ac12212f98
David P. Bauer <david@scholr.io>
2018-01-10 10:26:52 -0500

    Restyled app cards in store
    Used bootstrap grid to restyle the apps in the store and removed
    the waterfall library. Apps are added in rows of three columns.

--- store/index.php
+++ store/index.php
@@ -19,10 +19,17 @@ $p = $CFG->dbprefix;
 $OUTPUT->header();
 ?>
 <style>
-    .card {
-        border: 1px solid black;
-        margin: 5px;
-        padding: 5px;
+    .panel-default {
+        box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+    }
+    .panel-default:hover{
+        border: 1px solid #aaa;
+    }
+    .approw {
+        margin: 0;
+    }
+    .appcolumn {
+        padding: 0 4px;
     }
 #loader {
       position: fixed;
@@ -118,6 +125,14 @@ echo(settings_status($key_count));
     $count = 0;
     foreach($registrations as $name => $tool ) {
 
+        // This will keep the rows nice
+        if ($count % 3 == 0) {
+            if ($count > 0) {
+                echo('</div>');
+            }
+            echo('<div class="row approw">');
+        }
+
         $title = $tool['name'];
         $text = $tool['description'];
         $fa_icon = isset($tool['FontAwesome']) ? $tool['FontAwesome'] : false;
@@ -126,43 +141,38 @@ echo(settings_status($key_count));
             $icon = $CFG->fontawesome.'/png/'.str_replace('fa-','',$fa_icon).'.png';
         }
 
-        echo('<div style="border: 2px, solid, red;" class="card">');
+        echo('<div class="col-sm-4 appcolumn">
+        <div class="panel panel-default">
+        <div class="panel-body">');
         if ( $fa_icon ) {
             echo('<a href="details/'.urlencode($name).'">');
             echo('<i class="fa '.$fa_icon.' fa-2x" style="color: #1894C7; float:right; margin: 2px"></i>');
             echo('</a>');
         }
-        echo('<p><strong>'.htmlent_utf8($title)."</strong></p>");
-        echo('<p>'.htmlent_utf8($text)."</p>\n");
-        echo('<center><a href="details/'.urlencode($name).'" class="btn btn-default" role="button">Details</a> ');
+        echo('<h3 style="margin-top:.5em;">'.htmlent_utf8($title)."</h3>");
+        echo('<p>'.htmlent_utf8($text)."</p>\n
+            </div><div class=\"panel-footer\">");
+        echo('<a href="details/'.urlencode($name).'" class="btn btn-default" role="button">Details</a> ');
 
         $ltiurl = $tool['url'];
         if ( isset($_SESSION['gc_count']) ) {
             echo('<a href="'.$CFG->wwwroot.'/gclass/assign?lti='.urlencode($ltiurl).'&title='.urlencode($tool['name']));
             echo('" title="Install in Classroom" target="iframe-frame"'."\n");
-            echo("onclick=\"showModalIframe(this.title, 'iframe-dialog', 'iframe-frame', _TSUGI.spinnerUrl, true);\" >\n");
+            echo("onclick=\"showModalIframe(this.title, 'iframe-dialog', 'iframe-frame', _TSUGI.spinnerUrl, true);\" class=\"pull-right\">\n");
             echo('<img height=32 width=32 src="https://www.gstatic.com/classroom/logo_square_48.svg"></a>'."\n");
         }
-        echo('</center>');
-        echo("</div>\n");
+        echo("</div></div></div>\n");
 
         $count++;
     }
     if ( $count < 1 ) {
         echo("<p>No available tools</p>\n");
+    } else {
+        echo("</div>");
     }
 
 echo("</div>\n");
 // echo("<pre>\n");print_r($tool);echo("</pre>\n");
 
 $OUTPUT->footerStart();
-// https://github.com/LinZap/jquery.waterfall
-?>
-<script type="text/javascript" src="<?= $CFG->staticroot ?>/js/waterfall-light.js"></script>
-<script>
-$(function(){
-    $('#box').waterfall({refresh: 0})
-});
-</script>
-<?php
 $OUTPUT->footerend();
