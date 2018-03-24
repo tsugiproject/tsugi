cde7a94b7f7991f9eb9bc4572442510e917e3266
Charles R. Severance <csev@umich.edu>
2018-03-24 15:44:57 +0000

    More on mailtest

--- admin/testmail.php
+++ admin/testmail.php
@@ -16,8 +16,11 @@ if ( U::get($_POST,'email') && U::get($_POST,'subject') && U::get($_POST,'body')
     $subject =  U::get($_POST,'subject');
     $body = U::get($_POST,'body');
     $retval = Mail::send($to, $subject, $body);
-var_dump($retval);
-die();
+    if ( $retval ) {
+        $_SESSION['success'] = 'PHP mail() returned true';
+    } else {
+        $_SESSION['error'] = 'PHP mail() returned false';
+    }
     header("Location: testmail.php");
     return;
 }
@@ -33,23 +36,34 @@ $OUTPUT->flashMessages();
 <p>
 <form method="POST">
 <p>
-Mail address:
-<input type="email" name="email">
-</p>
-<p>
-Subject:
-<input type="text" name="subject">
+Mail address:<br/>
+<input style="width: 90%" type="email" name="email">
 </p>
 <p>
 Subject:<br/>
-<textarea name="body">
+<input style="width: 90%" type="text" name="subject">
+</p>
+<p>
+Subject:<br/>
+<textarea style="width: 90%; height: 150px;" name="body">
 </textarea>
 </p>
 <p>
-<input type="submit" onclick="$('#myspinner').show();return true;" name="delete" value="Delete Records"/>
+<input type="submit" onclick="$('#myspinner').show();return true;" name="delete" value="Send Mail"/>
 <img id="myspinner" src="<?= $OUTPUT->getSpinnerUrl() ?>" style="display:none">
 </p>
 </form>
+<p>
+This uses the PHP mail() function to send mail.  If mail sending does not work,
+it can be a PHP misconfiguration or a system misconfiguration. 
+<ul>
+<li>If this test returns 'false', it means PHP refused to send the mail.
+<li>If this test returns 'true', it means that PHP thought it sent the mail.  If the mail
+does not arrive after a few minutes it means the underlying operating system
+or environment did not spool the mail out.   You need to investigate things like
+postfix or sendmail or even SES on Amazon to get outbound mail flowing.
+</li>
+</ul>
 </p>
 <?php
 $OUTPUT->footer();
