da75fe62615163a550127085a1d10ef27e1db7e5
Charles Severance <csev@umich.edu>
2019-07-21 22:20:38 -0400

    Extend the menus with attributes.

--- vendor/tsugi/lib/src/UI/Menu.php
+++ vendor/tsugi/lib/src/UI/Menu.php
@@ -39,13 +39,14 @@ class Menu {
      * @param $link The text of the link - can be text, HTML, or even an img tag
      * @param $href An optional place to go when the link is clicked.  Also can be
      * a Menu.
-     * @param $push true if this is to be put before the rest of the items in the menue
-     *
+     * @param $push true if this is to be put before the rest of the items in the menu
+     * @param $attr An optional string to add within the anchor tag
+
      * @return Menu The instance is returned to allow chaining syntax
      */
-    public function addLink($link, $href, $push=false)
+    public function addLink($link, $href, $push=false, $attr=false)
     {
-        $entry = new MenuEntry($link, $href);
+        $entry = new MenuEntry($link, $href, $attr);
         return $this->add($entry, $push);
     }
 
