69620b371068ccc651724d885fee6e3b38f60362
Charles Severance <csev@umich.edu>
2020-09-08 14:23:38 -0400

    Updates

--- lti/store/sakai-config.php
+++ lti/store/sakai-config.php
@@ -35,5 +35,24 @@ if ( isset($CFG->servicedesc) && $CFG->servicedesc ) {
     $json->description = $CFG->servicedesc;
 }
 
+/* From IMS */
+
+$json->application_type = "web";
+$json->token_endpoint_auth_method = "private_key_jwt";
+$json->response_types = array("id_token");
+$json->grant_types = array("implicit", "client_credentials");
+
+$json->iniate_login_uri = $CFG->wwwroot . '/lti/oidc_login/' . urlencode($guid);
+$json->redirect_uris = array($CFG->wwwroot . '/lti/oidc_launch');
+$json->jwks_uri = $CFG->wwwroot . '/lti/keyset/' . urlencode($guid);
+
+if ( isset($CFG->owneremail) && $CFG->owneremail ) {
+    $json->contacts = array($CFG->owneremail);
+    $contact = new \stdClass();
+    $contact->email = $CFG->owneremail;
+    if ( isset($CFG->ownername) && $CFG->ownername ) $contact->display_name = $CFG->ownername;
+    $json->better_contacts = array($contact);
+}
+
 header("Content-type: application/json");
 echo(json_encode($json, JSON_PRETTY_PRINT));
