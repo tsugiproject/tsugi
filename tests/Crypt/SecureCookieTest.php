6c1ef425113da5b1d83d6f6fadaadc0efc499001
Charles Severance <csev@umich.edu>
2020-09-09 06:39:58 -0400

    Extract encrypt and decrypt into their own methods

--- tests/Crypt/SecureCookieTest.php
+++ tests/Crypt/SecureCookieTest.php
@@ -11,10 +11,26 @@ require_once "src/Config/ConfigInfo.php";
 
 class SecureCookieTest extends PHPUnit_Framework_TestCase
 {
+    public function testAESCookie() {
+        global $CFG;
+        $CFG = new \Tsugi\Config\ConfigInfo(basename(__FILE__),'http://localhost');
+        $CFG->cookiepad = 'Helloworld';
+        $CFG->cookiesecret = '42';
+        $pt = 'HHGTTG';
+        $expected = 'BwHVfvOvWF9+H9K0arM=';
+        $ct = SecureCookie::encrypt($pt);
+        $this->assertNotEquals($pt, $ct);
+        // You can't assertEquals on ct because the encryption is never the same twice
+        $this->assertEquals(strlen($ct), strlen($expected));
+        $pt2 = SecureCookie::decrypt($ct);
+        $this->assertEquals($pt, $pt2);
+    }
+
     public function testSecureCookie() {
         global $CFG;
         $CFG = new \Tsugi\Config\ConfigInfo(basename(__FILE__),'http://localhost');
         $CFG->cookiepad = 'Helloworld';
+        $CFG->cookiesecret = '42';
         $id = 1;
         $guid = 'xyzzy';
         $cid = '999';
