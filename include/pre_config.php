5fdf6bb6066874fb792f80536d256f1ca1584d2a
David Bauer <dbauer1@udayton.edu>
2021-01-11 21:36:00 -0500

    Another typo

--- include/pre_config.php
+++ include/pre_config.php
@@ -3,4 +3,4 @@
 // Setup I18N before autoload to prevent Laravel helpers from overriding translation function __()
 // These two lines must be before autoload.php
 require_once (__DIR__."/../src/Core/I18N.php");
-require_once (__DIR__."setup_i18n.php");
+require_once (__DIR__."/setup_i18n.php");
