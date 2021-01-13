4d304e7c037bb585e71e84d1ae0ae580a0f20751
Charles Severance <csev@umich.edu>
2021-01-12 19:37:18 -0500

    fix unit test

--- tests/UI/HandleBarsTest.php
+++ tests/UI/HandleBarsTest.php
@@ -2,15 +2,19 @@
 
 $toppath = dirname(__FILE__).'/../..';
 
-require_once "include/pre_config.php";
+// require_once "include/pre_config.php";
+require_once "src/Core/I18N.php";
 require_once "src/UI/HandleBars.php";
-require_once $toppath.'/vendor/symfony/translation/TranslatorBagInterface.php';
-require_once $toppath.'/vendor/symfony/translation/TranslatorInterface.php';
-require_once $toppath.'/vendor/symfony/translation/Translator.php';
-require_once $toppath.'/vendor/symfony/translation/MessageSelector.php';
-require_once $toppath.'/vendor/symfony/translation/MessageCatalogueInterface.php';
-require_once $toppath.'/vendor/symfony/translation/MetadataAwareInterface.php';
-require_once $toppath.'/vendor/symfony/translation/MessageCatalogue.php';
+
+// require_once $toppath.'/vendor/symfony/translation/Translator.php';
+// require_once $toppath.'/vendor/symfony/translation/TranslatorInterface.php';
+// require_once $toppath.'/vendor/symfony/translation/TranslatorBagInterface.php';
+// require_once $toppath.'/vendor/symfony/translation/TranslatorInterface.php';
+// require_once $toppath.'/vendor/symfony/translation/Translator.php';
+// require_once $toppath.'/vendor/symfony/translation/MessageSelector.php';
+// require_once $toppath.'/vendor/symfony/translation/MessageCatalogueInterface.php';
+// require_once $toppath.'/vendor/symfony/translation/MetadataAwareInterface.php';
+// require_once $toppath.'/vendor/symfony/translation/MessageCatalogue.php';
 
 use \Tsugi\UI\HandleBars;
 
