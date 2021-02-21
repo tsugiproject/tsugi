6524eb737ef175629a16093f8e0763e207b97683
Charles Severance <csev@umich.edu>
2021-02-21 12:08:51 -0500

    Add the file - properly named.

---
+++ tests/Core/JsonTraitTest.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once("src/Core/JSONTrait.php");
+use \Tsugi\Core\JsonTrait;
+
+class JsonTraitTest extends \PHPUnit\Framework\TestCase
+{
+    public function testTrivial() {
+        $this->assertTrue(true);
+    }
+
+}
