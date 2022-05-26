f0b8bd07786ab2411ddd2e02f4ca051953790989
TurRil <lightscape2001@gmail.com>
2022-05-26 13:25:15 +0200

    Also applies to htmlspecialchars

--- vendor/tsugi/lib/src/Util/U.php
+++ vendor/tsugi/lib/src/Util/U.php
@@ -45,7 +45,7 @@ class U {
     }
 
     public static function htmlspec_utf8($string) {
-        if ( is_string($string) ) return htmlspecialchars($string,ENT_QUOTES,$encoding = 'UTF-8');
+        if ( is_string($string) ) return htmlspecialchars($string ?? '',ENT_QUOTES,$encoding = 'UTF-8');
         return $string;
     }
 
@@ -384,7 +384,7 @@ class U {
         if ( $CFG->DEVELOPER === TRUE ) {
             if ( strlen($DEBUG_STRING) > 0 ) {
                 echo("\n<pre>\n");
-                echo(htmlentities($DEBUG_STRING));
+                echo(htmlentities($DEBUG_STRING ?? ''));
                 echo("\n</pre>\n");
             }
         }
