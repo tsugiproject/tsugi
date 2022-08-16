ca8cfa1bdba2c97201509d4434c6834700b1bf62
Charles Severance <csev@umich.edu>
2022-08-15 23:15:10 -0400

    Update dependencies

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -25,42 +25,19 @@ class ComposerAutoloaderInit129cbc536a214697caadba3ab28a195d
         require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInit129cbc536a214697caadba3ab28a195d', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit129cbc536a214697caadba3ab28a195d', 'loadClassLoader'));
 
         $includePaths = require __DIR__ . '/include_paths.php';
         $includePaths[] = get_include_path();
         set_include_path(implode(PATH_SEPARATOR, $includePaths));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
-        if ($useStaticLoader) {
-            require __DIR__ . '/autoload_static.php';
-
-            call_user_func(\Composer\Autoload\ComposerStaticInit129cbc536a214697caadba3ab28a195d::getInitializer($loader));
-        } else {
-            $map = require __DIR__ . '/autoload_namespaces.php';
-            foreach ($map as $namespace => $path) {
-                $loader->set($namespace, $path);
-            }
-
-            $map = require __DIR__ . '/autoload_psr4.php';
-            foreach ($map as $namespace => $path) {
-                $loader->setPsr4($namespace, $path);
-            }
-
-            $classMap = require __DIR__ . '/autoload_classmap.php';
-            if ($classMap) {
-                $loader->addClassMap($classMap);
-            }
-        }
+        require __DIR__ . '/autoload_static.php';
+        call_user_func(\Composer\Autoload\ComposerStaticInit129cbc536a214697caadba3ab28a195d::getInitializer($loader));
 
         $loader->register(true);
 
-        if ($useStaticLoader) {
-            $includeFiles = Composer\Autoload\ComposerStaticInit129cbc536a214697caadba3ab28a195d::$files;
-        } else {
-            $includeFiles = require __DIR__ . '/autoload_files.php';
-        }
+        $includeFiles = \Composer\Autoload\ComposerStaticInit129cbc536a214697caadba3ab28a195d::$files;
         foreach ($includeFiles as $fileIdentifier => $file) {
             composerRequire129cbc536a214697caadba3ab28a195d($fileIdentifier, $file);
         }
--- vendor/tsugi/lib/src/Util/CC.php
+++ vendor/tsugi/lib/src/Util/CC.php
@@ -235,7 +235,7 @@ class CC extends \Tsugi\Util\TsugiDOM {
     /**
      * Add a resource to the manifest.
      */
-    public function add_resource_item($module, $title=null, $type, $identifier, $file) {
+    public function add_resource_item($module, $title, $type, $identifier, $file) {
         $this->last_file = $file;
         $this->last_type = $type;
         $this->last_identifier = $identifier;
