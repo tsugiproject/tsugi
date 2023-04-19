fffc370eccc227f124a782bde1f3f1bbce91f42d
Charles Severance <csev@umich.edu>
2023-04-19 19:30:31 -0400

    Initialize extensions in constructor

--- src/Config/ConfigInfo.php
+++ src/Config/ConfigInfo.php
@@ -16,7 +16,7 @@ class ConfigInfo {
     /**
      * Extensions
      */
-    public array $extensions = array();
+    public $extensions;
 
     /**
      * The URL where tsugi is located
@@ -705,6 +705,7 @@ class ConfigInfo {
     public function __construct($dirroot, $wwwroot, $dataroot=false) {
         $this->dirroot = $dirroot;
         $this->wwwroot = $wwwroot;
+        $this->extenstions = array();
         $this->staticroot = 'https://static.tsugi.org';
         $this->lumen_storage = sprintf("%s/storage/", $dirroot);
     }
