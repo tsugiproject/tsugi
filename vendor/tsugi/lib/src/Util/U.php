81b1013007e5a8c13284609f1b6fe1799ac1237b
Charles Severance <csev@umich.edu>
2023-08-25 18:06:48 -0400

    Fix empty() again

--- vendor/tsugi/lib/src/Util/U.php
+++ vendor/tsugi/lib/src/Util/U.php
@@ -368,7 +368,7 @@ class U {
     }
 
     public static function isCli() {
-         if(php_sapi_name() == 'cli' && self::isEmpty($_SERVER['REMOTE_ADDR'])) {
+         if(php_sapi_name() == 'cli' && empty($_SERVER['REMOTE_ADDR'])) {
               return true;
          } else {
               return false;
