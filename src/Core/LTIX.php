4026482c929ed93126998bed664f1461587d1d93
Charles Severance <csev@umich.edu>
2023-09-15 22:30:49 -0400

    Fix from David / Andrew

--- src/Core/LTIX.php
+++ src/Core/LTIX.php
@@ -3218,4 +3218,27 @@ class LTIX {
     public static function getKidForKey($pubkey) {
         return hash('sha256', trim($pubkey));
     }
+
+    /** Retrieve the context and link ids for a previous installation of the tool, such as
+     *  if it were imported from another site during a site import. Returns false, if not.
+     * @return array[
+     *  'context_id' => number,
+     *  'link_id' => number
+     * ] | false
+     */
+    public static function getLatestHistoryIds() {
+        global $CFG, $LAUNCH, $PDOX;
+        $resource_history = $LAUNCH->ltiCustomGet("resourcelink_id_history", '$ResourceLink.id.history');
+        if ($resource_history != '$ResourceLink.id.history') {
+            // Split the link_key and use the most recent one
+            $link_key_arr = explode(',', $resource_history);
+            $prev_link_key = end($link_key_arr);
+            // Get the previous context and link ids
+            $linkquery = "SELECT context_id, link_id FROM {$CFG->dbprefix}lti_link WHERE link_key = :link_key;";
+            $linkarr = array(':link_key' => $prev_link_key);
+            return $PDOX->rowDie($linkquery, $linkarr);
+        } else {
+            return false;
+        }
+    }
 }
