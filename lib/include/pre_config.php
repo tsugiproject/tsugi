a354ffcc72ffaaebce524ca29221df81c24d48ce
Charles Severance <csev@umich.edu>
2026-03-04 13:16:06 -0500

    Change pre-config and new name for cookie

--- lib/include/pre_config.php
+++ lib/include/pre_config.php
@@ -1,6 +1,8 @@
 <?php
 
+// Commented out 2026-03-04 - No more Laravel
+
 // Setup I18N before autoload to prevent Laravel helpers from overriding translation function __()
 // These two lines must be before autoload.php
-require_once (__DIR__."/../src/Core/I18N.php");
-require_once (__DIR__."/setup_i18n.php");
+// require_once (__DIR__."/../src/Core/I18N.php");
+// require_once (__DIR__."/setup_i18n.php");
