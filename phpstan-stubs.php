99db8ef7339d9d6449ddedf0ab098ee1ec82c218
Charles Severance <csev@umich.edu>
2026-03-05 16:50:37 -0500

    Make tests work.

--- phpstan-stubs.php
+++ phpstan-stubs.php
@@ -5,6 +5,10 @@
  */
 
 namespace {
+    const TSUGI_COOKIELESS_SESSION_NAME = "_LTI_TSUGI";
+    const TSUGI_SESSION_LTI = 'lti';
+    const TSUGI_SESSION_LTI_POST = 'lti_post';
+
     /** @var \Tsugi\Config\ConfigInfo|null $CFG */
     $CFG;
 
