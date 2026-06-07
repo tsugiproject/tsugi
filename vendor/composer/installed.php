5c8f0efaab3c4367fc8b66f619f57e47c82db772
Charles Severance <csev@umich.edu>
2026-06-07 15:02:02 -0400

    Add php8.5 to PHPStan

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '739cbe1693041371bdcb2e24efffcec3d4a20851',
+        'reference' => 'a5b4813d50096202af18f08d2437e563fa15778b',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '739cbe1693041371bdcb2e24efffcec3d4a20851',
+            'reference' => 'a5b4813d50096202af18f08d2437e563fa15778b',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
